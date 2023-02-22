77fa36a2c76401d8db896b7cc5d1e9de6d23480f
scorpionalan <alan_torres_mora@comunidad.unam.mx>
2023-02-22 10:05:54 -0600

    TAREA2

---
+++ CODIGO/ALAN_TORRES_POO/src/ACSII_TAREA.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class ACSII_TAREA {
+    public static void main(String[] args) {
+        Scanner entrada= new Scanner (System.in);
+        String ingreso;
+        boolean valor=true;
+        System.out.println("Introduzca valores: ");
+        ingreso= entrada.nextLine();
+        for (int i=0; i<ingreso.length()-1;i++){
+            if ((int)ingreso.charAt(i+1)-(int)ingreso.charAt(i)==1){
+                valor=true;
+            }
+            else{
+                valor=false;
+                break;
+            }
+        }
+        System.out.println("La secuencia es: "+ valor);
+    }
+}
